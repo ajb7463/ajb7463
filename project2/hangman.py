20d450e949533c6226f52b39ba18712ce46a8aac
Alex Barr <ajb7463@psu.edu>
2020-12-17 02:59:37 +0000

    phase 1

--- project2/hangman.py
+++ project2/hangman.py
@@ -1,4 +1,3 @@
-from sys import argv
 
 def get_words(filename):
   words = []
@@ -14,11 +13,44 @@ def word_bank1(words):
       word1 = word
   return word1
 
+def hangmanp1(words):
+  wrongletters = ""
+  tries = int(argv[3])
+  hword = "_"*int(argv[2])
+  word1 = word_bank1(words)
+
+  while tries > 0 and  word1 != hword:
+    print(hword)
+    print(f'missed letters: {wrongletters} ({tries} chances left)')
+
+    attempt = input("Enter your guess: ")
+
+    if attempt in word1:
+      lword = list(hword)
+
+      for i in range(len(word1)):
+        if attempt == word1[i]:
+          lword.insert(i,attempt)
+          lword.pop(i + 1)
+      hword = ""
+      
+      for i in lword:
+        hword += i
+    
+    wrongletters = wrongletters + attempt + " "
+    tries -= 1
+
+  if word1 == hword:
+    print(f'You guessed the word: {word1}.')
+  else:
+    print(f'You lost after {argv[3]} wrong chances.')
+      
+  
 
 
 
 
-
+from sys import argv
 def debug_print(s):
   if len(argv) > 4:
     print(s)
