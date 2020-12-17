d74e843175c2c0ea15297e26a949f49c17685558
Alex Barr <ajb7463@psu.edu>
2020-12-17 02:43:50 +0000

    setup

--- project2/hangman.py
+++ project2/hangman.py
@@ -1,4 +1,24 @@
 from sys import argv
+
+def get_words(filename):
+  words = []
+
+  with open(filename) as fin:
+    for line in fin:
+      words.append(line.strip())
+  return words
+
+def word_bank1(words):
+  for word in words:
+    if len(word) == int(argv[2]):
+      word1 = word
+  return word1
+
+
+
+
+
+
 def debug_print(s):
   if len(argv) > 4:
     print(s)
